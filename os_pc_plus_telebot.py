8f68b549820243333b3e5204097be10c92e4450e
volemor <volemor@gmail.com>
2022-06-30 16:29:30 +0300

    add description

--- os_pc_plus_telebot.py
+++ os_pc_plus_telebot.py
@@ -85,7 +85,7 @@ def start(message):
                 kill_line += line.split()[0] + ' '
         print('kill_line:', kill_line)
         bot.send_message(message.chat.id, f'old process [{kill_line}] killed, make git pull and start new')
-        os.popen(f'kill {kill_line} && /root/bot1_start.bat').read()
+        os.popen(f'kill {kill_line} && nohup /root/bot1_start.bat & echo ok').read()
         # if 'bin_bot1.py' not in os.popen("ps -axf").read():
         #     os.popen('/root/bot1_start.bat').read()
         # for index in os.popen("ps -axf").read().split('\n'):
