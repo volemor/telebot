98ddc40ea614fbe1d7f26bb79a8128d263dbdfbd
volemor <volemor@gmail.com>
2022-07-04 12:46:08 +0300

    add exception log read

--- os_pc_plus_telebot.py
+++ os_pc_plus_telebot.py
@@ -98,7 +98,7 @@ def allrestart(message):
     if check_accesss(message.from_user.id):
         kill_line = ''
         for line in os.popen('ps -axf|grep .py').read().split('\n'):
-            if 'bin_bot1.py' in line or 'os_pc_plus_telebot.py'  in line or 'hist_uploader.py' in line:
+            if 'bin_bot1.py' in line or 'os_pc_plus_telebot.py' in line or 'hist_uploader.py' in line:
                 print(line.split()[0])
                 kill_line += line.split()[0] + ' '
         print('kill_line:', kill_line)
