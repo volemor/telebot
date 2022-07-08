23ee5cc9f1c8e73aba337bc79e8b83b209acae81
volemor <volemor@gmail.com>
2022-07-08 20:51:12 +0300

    update sendfile modul

--- my_os_test.py
+++ my_os_test.py
@@ -233,6 +233,7 @@ def sendmefile(message):
     else:
         bot.send_message(message.chat.id, f'Пожалуй тебя нет в списках.. id ={message.from_user.id}')
         from my_os_test_config import subscriber_list
+        sendmefile(message)
 
 while True:
     try:
