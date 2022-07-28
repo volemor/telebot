fa880454d805375fe76ab02b07222b8013a97fe2
Dmitry M <38764934+volemor@users.noreply.github.com>
2022-07-28 19:30:12 +0300

    add r_telebot

---
+++ telegramm_bot_for_riga.py
@@ -0,0 +1,83 @@
+# my telegramm bot
+"""
+телеграмм бот для мониторинга системы 'riga'
+команды бота - описание:
+start - ps axf|grep python3
+netstat - netstat 22 port
+
+"""
+
+import os
+import telebot
+from tendo import singleton
+import time
+from secret_bot import *
+
+me = singleton.SingleInstance()  ### проверка на работу и запуск альтернативной версии скрипта - чтоб не задвоялась
+
+# проверка собстенными силами.. не всегда может сработать - имя поменять и все..
+processoutput = os.popen("ps -axf").read()
+my_list = processoutput.split('\n')
+count_my_prog = 0
+for index in my_list:
+    if '.py' in index:
+        if 'telegramm_bot_for_riga.py' in index:
+            count_my_prog += 1
+        if count_my_prog == 2:
+            print('programm allready run!!!')
+            exit()
+
+
+
+bot = telebot.TeleBot(secrets_bot_token)
+""" бот для запуска на линуксе и мониторинга """
+
+
+def check_for_access(name):
+    if str(name) in my_access_list:
+        return True
+    else:
+        return False
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if check_for_access(message.from_user.id):
+        my_process_py = ''
+        processoutput = os.popen("ps -axf").read()
+        my_list = processoutput.split('\n')
+        for index in my_list:
+            if '.py' in index:
+                my_process_py += index + '\n'
+        bot.send_message(message.chat.id, my_process_py)
+    else:
+        bot.send_message(message.chat.id, 'запуск прошел успешно')
+
+
+@bot.message_handler(commands=['netstat'])
+def nenstat_status(message):
+    if check_for_access(message.from_user.id):
+        my_process_py = ''
+        processoutput = os.popen("netstat -antp").read()
+        my_list = processoutput.split('\n')
+        for index in my_list:
+            if 'ESTABLISHED' in index and ':22' in index:
+                my_process_py += index + '\n'
+        if len(my_process_py) == 0:
+            my_process_py = 'сейчас нет соединений по 22 порту'
+        bot.send_message(message.chat.id, my_process_py)
+    else:
+        bot.send_message(message.chat.id, 'нет соединений')
+
+
+def save_exeption(_ex: str):
+    with open('telebot_ex.log', 'a') as file:
+        file.writelines(_ex + '\n')
+
+
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception as _ex:
+        save_exeption(_ex)
+    time.sleep(100)
