6501c3ba89b50d6a1e550e0b5879bd4aec86f5f1
Dmitry M <38764934+volemor@users.noreply.github.com>
2022-07-28 20:45:07 +0300

    add auth log analyser

--- telegramm_bot_for_riga.py
+++ telegramm_bot_for_riga.py
@@ -13,7 +13,7 @@ allrestart- old process killed, make git pull and start new
 import os
 import telebot
 from tendo import singleton
-import time
+import time, datetime
 from secret_bot import secrets_bot_token, my_access_list
 
 me = singleton.SingleInstance()  ### проверка на работу и запуск альтернативной версии скрипта - чтоб не задвоялась
@@ -76,7 +76,7 @@ def allrestart(message):
 
 
 @bot.message_handler(commands=['test'])
-def allrestart(message):
+def test_test(message):
     if check_for_access(message.from_user.id):
         bot.send_message(message.chat.id, f'you in access list')
     else:
@@ -86,6 +86,64 @@ def allrestart(message):
 def save_exeption(_ex: str):
     with open('telebot_ex.log', 'a') as file:
         file.writelines(_ex + '\n')
+
+
+@bot.message_handler(commands=['auth_log'])
+def auth_log(message):
+    if check_for_access(message.from_user.id):
+        my_mess = auth_log_analyser(message)
+        bot.send_message(message.chat.id, f'{my_mess}')
+    else:
+        bot.send_message(message.chat.id, f'not for you')
+
+
+def auth_log_analyser(message):
+    count_fail = 0
+    fail_list = []
+    filename = '/var/log/auth.log'
+    log_list = os.listdir('/var/log/')
+    if 'auth.log' in log_list:
+        with open(filename, mode='r') as file:
+            for line in file:
+                if 'Failed password' in line:
+                    fail_list.append([*line.split()[:3], line.split()[-4]])
+                    count_fail += 1
+    else:
+        return 'log file not found'
+
+    hack_count = {ip_item[3]: 0 for ip_item in fail_list}
+    for my_item in hack_count.keys():
+        counting = 0
+        for list_item in fail_list:
+            if my_item in list_item:
+                counting += 1
+
+        hack_count[my_item] = counting
+    hach_ip_message = 'auth.log status:'
+    for h_key, h_item in hack_count.items():
+        hach_ip_message += f'hackers ip [{h_key}] try [{h_item}]\n'
+        # print(f'hackers ip [{h_key}] try [{h_item}]')
+    hach_ip_message += f'try [{count_fail}] from unique address [{len(hack_count)}]\n\n'
+    # print(f'try [{count_fail}] from unique address [{len(hack_count)}]')
+
+    warning_status = []
+    for my_item in hack_count.keys():
+        delta_time = []
+        for time in fail_list:
+            if my_item in time[3]:
+                delta_time.append(time)
+        for index_d in range(len(delta_time) - 1):
+            h, m, s = delta_time[index_d][2].split(':')
+            hl, ml, sl = delta_time[index_d + 1][2].split(':')
+            if (datetime.time(hour=int(hl), minute=int(ml), second=int(sl)).second
+                - datetime.time(hour=int(h), minute=int(m), second=int(s)).second) < 4:
+                warning_status.append((my_item, delta_time[index_d][:3]))
+        # print('row time---', my_item, delta_time)
+    # print('warn', warning_status)
+    for hack_ip, h_time in warning_status:
+        hach_ip_message += f'IP:{hack_ip}:{h_time}\n'
+        print(hack_ip, h_time)
+    return hach_ip_message
 
 
 while True:
