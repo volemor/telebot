272a0c135ecc6ecdc32137a851f20dd47f66162a
Dmitry M <38764934+volemor@users.noreply.github.com>
2022-07-30 21:36:11 +0300

    add sendme

--- my_os_test.py
+++ my_os_test.py
@@ -302,7 +302,7 @@ def sendme(message: Message):
         itembtnQ = types.KeyboardButton('?')
         markup.row(itembtna, itembtnd, itembtnQ)
         bot.send_message(message.from_user.id, "Choose one letter:", reply_markup=markup)
-
+        bot.reply_to(message.chat.id, 'any')
         if message.text == 'd':
             bot.send_message(message.chat.id, 'you --- try d ')
         spl = message.text.split()
