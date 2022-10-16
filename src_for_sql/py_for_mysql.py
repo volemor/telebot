0985cc3fd6dd7ae42487ca6bb017084b31bd0cfa
Dmitry M <38764934+volemor@users.noreply.github.com>
2022-10-16 13:25:37 +0300

    add script for mysql data base

--- src_for_sql/py_for_mysql.py
+++ src_for_sql/py_for_mysql.py
@@ -3,15 +3,17 @@ import os
 from datetime import datetime
 
 today = datetime.today().date()
+save_dir = '/mnt/60G'
+file_name = ['hass_db', 'hist_data']
 
 print(today)
-command = '/usr/bin/mysqldump  -uroot -pbananapi hass_db > /mnt/60G/hassio_db-' + str(today) + '.sql'
-os.system(command)
-command1 = '/usr/bin/mysqldump  -uroot -pbananapi hist_data > /mnt/60G/hist_data-' + str(today) + '.sql'
-os.system(command1)
+for base_name in file_name:
+    command = f'/usr/bin/mysqldump  -uroot -pbananapi {base_name} > {save_dir}/{base_name}-{today}.sql'
+    os.system(command)
+# command1 = '/usr/bin/mysqldump  -uroot -pbananapi hist_data > /mnt/60G/hist_data-' + str(today) + '.sql'
+# os.system(command1)
 
-save_dir = '/mnt/60G'
-file_name = ['hassio_db', 'hist_data']
+
 file_list = {}
 for item in file_name:
     file_list[item] = [name for name in os.listdir(save_dir) if os.path.isfile(name) and item in name]
