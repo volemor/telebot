75cd3a51d4087ce19fd61454565170645d6b8c2f
Dmitry M <38764934+volemor@users.noreply.github.com>
2022-10-18 23:02:42 +0300

    add run command script

--- src_for_banan/py_run_check.py
+++ src_for_banan/py_run_check.py
@@ -15,5 +15,5 @@ if run_process_name[0] not in com_line:
     print('Start 0')
 
 if run_process_name[1] not in com_line:
-    mess = os.popen('nohup python3 /root/my_py/telebot/ban_monitor/menu_bot.py ').read()
+    mess = os.popen('nohup cd /root/my_py/telebot/ban_monitor && python3 menu_bot.py ').read()
     print('Start 1', mess)
