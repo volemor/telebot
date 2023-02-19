1583f1c91314ef1b599f0eb72ab0c499f0f69f28
Dmitry M <38764934+volemor@users.noreply.github.com>
2023-02-19 16:51:17 +0300

    add rimm py_run_check.py

---
+++ src_for_rimm/py_run_check.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# coding: utf-8
+# for rimm
+
+import os
+
+print("RUN check start!!!")
+
+run_process_name = ['menu_bot.py']
+com_line = os.popen('ps axf|grep .py').read()
+# print(com_line)
+
+if run_process_name[0] not in com_line:
+    os.popen('cd /home/dmitry/1T/py_proj/telebot && nohup python3 menu_bot.py')
+    print('Start 0')
+#
+# if run_process_name[1] not in com_line:
+#     mess = os.popen('cd /root/my_py/telebot/ban_monitor && nohup python3 menu_bot.py').read()
+#     print('Start 1', mess)
+#
+# if 'cheker_mes_bot.py' not in com_line:
+#     mess = os.popen('cd /root/my_py/telebot/ban_monitor && nohup python3 cheker_mes_bot.py').read()
+#     print('Start 2', mess)
