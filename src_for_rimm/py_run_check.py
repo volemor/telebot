ce904987e1dfeedc944a864e671898d73ae1c006
Dmitry M <38764934+volemor@users.noreply.github.com>
2023-03-13 20:31:43 +0300

    add rimm py_run_check.py

--- src_for_rimm/py_run_check.py
+++ src_for_rimm/py_run_check.py
@@ -11,12 +11,12 @@ com_line = os.popen('ps axf|grep .py').read()
 # print(com_line)
 
 if run_process_name[0] not in com_line:
-    os.popen('cd /home/dmitry/1T/py_proj/telebot && nohup python3 menu_bot.py')
+    os.popen('cd /opt/Py_proj/telebot && nohup python3 menu_bot.py')
     print(f'Start 0:{run_process_name[0]}' )
 
 
 if run_process_name[1] not in com_line:
-    mess = os.popen('cd /home/dmitry/1T/py_proj/telebot && nohup python3 divTelebot.py').read()
+    mess = os.popen('cd /opt/Py_proj/telebot && nohup python3 divTelebot.py').read()
     print(f'Start 1:{run_process_name[1]}\n', mess)
 
 # if 'cheker_mes_bot.py' not in com_line:
